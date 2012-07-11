5badf33f961ddfed8335ffc6327577ccf4fd5834
Spencer Schack <scs1818@gmail.com>
2012-07-10 17:51:16 -0700

    changed color order

--- app/helpers/fields_helper.rb
+++ app/helpers/fields_helper.rb
@@ -4,11 +4,11 @@ module FieldsHelper
   
   FIELDS[:fields] = {
     index: [:column, :template],
-    show: [:column, :x, :y, :width, :height, :font, :text_size, :color,
+    show: [:column, :x, :y, :width, :height, :font, :color, :text_size,
       :align, :template],
     form: [[:column, collection: Student.template_columns],
       [:x, hint: 'from the left'], [:y, hint: 'from the bottom'], :width,
-      :height, :font, :text_size, :color,
+      :height, :font, :color, :text_size,
       [:align, collection: Field.align_options], :template]
   }
   
