b34ffeb9efca137a1315e759a061d3fc6ac18eb1
Spencer Schack <scs1818@gmail.com>
2012-08-06 13:02:02 -0700

    added sorting of test attributes on test scores index

--- app/controllers/test_scores_controller.rb
+++ app/controllers/test_scores_controller.rb
@@ -26,16 +26,17 @@ class TestScoresController < ApplicationController
         .includes(:test_group, :test_attributes)
       
       @test_model_indices = {}
+      @test_attribute_indices = {}
       @cell_attributes = []
       @column_total = @test_models.reduce(0) do |index, model|
-        length = index + model.test_attributes.length
-        
-        @cell_attributes[index] = "class=\"parent\" data-id=\"#{model.id}\"".html_safe
-        child_attributes = "class=\"child\" data-parent-id=\"#{model.id}\"".html_safe
-        @cell_attributes.fill(child_attributes, index + 1, length)
-        
         @test_model_indices[model.id] = index
-        length + 1
+        @cell_attributes[index] = "class=\"parent\" data-id=\"#{model.id}\"".html_safe
+        ordered_attributes(model.test_attributes).each do |attribute|
+          @test_attribute_indices[attribute.id] = (index += 1)
+          @cell_attributes[index] = "class=\"child\" data-parent-id=\"#{model.id}\"".html_safe
+          @cell_attributes[index][11] += ' overall' unless attribute.parent_id?
+        end
+        index + 1
       end
     end
   end
@@ -48,6 +49,23 @@ class TestScoresController < ApplicationController
   def dynamic_fields_member
     @hide_fields = params[:test_model_id].blank?
     render '_dynamic_fields', layout: false
+  end
+  
+  private
+  
+  def ordered_attributes test_attributes
+    grouped = test_attributes.group_by(&:parent_id)
+    ordered = Array.new(test_attributes.length)
+    index = -1
+    grouped[nil].sort_by(&:name).each do |parent_attribute|
+      ordered[index += 1] = parent_attribute
+      if children = grouped[parent_attribute.id]
+        children.sort_by(&:name).each do |child_attribute|
+          ordered[index += 1] = child_attribute
+        end
+      end
+    end
+    ordered
   end
 
 end
--- app/helpers/test_scores_helper.rb
+++ app/helpers/test_scores_helper.rb
@@ -6,13 +6,25 @@ module TestScoresHelper
     show: { fields: [:term], relations: [:student, :test_model] }
   }
   
+  def arranged_attributes test_models
+    ordered = Array.new(@column_total)
+    test_models.each do |model|
+      parents = model.test_attributes.reject{|attribute| attribute.parent_id }
+      ordered[@test_model_indices[model.id]] = model
+      model.test_attributes.each do |attribute|
+        ordered[@test_attribute_indices[attribute.id]] = attribute
+      end
+    end
+    ordered
+  end
+  
   def arranged_values test_scores
     ordered = Array.new(@column_total)
     test_scores.each do |score|
-      index = @test_model_indices[score.test_model_id]
-      ordered[index] = score.test_values.reject{|value| value.test_attribute.parent_id }
+      parents = score.test_values.reject{|value| value.test_attribute.parent_id }
+      ordered[@test_model_indices[score.test_model_id]] = parents
       score.test_values.each do |value|
-        ordered[index += 1] = value
+        ordered[@test_attribute_indices[value.test_attribute_id]] = value
       end
     end
     ordered
