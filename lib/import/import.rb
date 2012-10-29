c1034b22096340c4b72cc6be5d73b24ab9b49d67
Spencer Schack <scs1818@gmail.com>
2012-10-28 17:55:43 -0700

    changed student identifier scope back, i can't deal with this shit right now

--- lib/import/import.rb
+++ lib/import/import.rb
@@ -123,11 +123,11 @@ class Import
       if hash[identifier]
         finder = model
         Array.wrap(scopes).compact.each do |scope|
-          if scope_value = (hash[scope] || hash["#{scope}_id"])
+          if !hash[scope]
             raise ArgumentError, "To find a #{model.name.titleize.downcase} by" <<
               " #{identifier}, you must also enter a #{scope.to_s.humanize.downcase}"
           else
-            finder = finder.where(scope.to_sym => scope_value)
+            finder = finder.where(scope.to_sym => hash[scope])
           end
         end
         record = finder.send("find_by_#{identifier}", hash[identifier])
