ad8d5d8b498a17d986264b487d1fe117ff83260f
Spencer Schack <scs1818@gmail.com>
2012-12-27 13:18:14 -0800

    removed expiring url from Paperclip initializer because caching broke the expiring urls

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,13 +1,3 @@
-module Paperclip
-  class Attachment
-    
-    def url style_name = default_style, options = {}
-      expiring_url(60, style_name)
-    end
-    
-  end
-end
-
 Paperclip.interpolates :style_unless_original do |attachment, style|
   "-#{style}" unless style == :original
 end
