d5b397a395954248058b1e62554b732f36973db9
Spencer Schack <scs1818@gmail.com>
2013-01-06 15:21:54 -0800

    removed default image from student

--- app/models/student.rb
+++ app/models/student.rb
@@ -36,8 +36,7 @@ class Student < ActiveRecord::Base
       original: ['', :jpg],
       thumbnail: ['56x70', :jpg],
       index: ['28x35', :jpg]
-    },
-    default_url: 'http://placehold.it/28x35'
+    }
   
   has_import identify_with: { identifier: :school_id }, associate: { school: :identifier,
     bus_route: :name, bus_stop: :name },
