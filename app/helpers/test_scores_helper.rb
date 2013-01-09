b2dc5a1d9c05a1b635af73f35732c321c4b36f1a
Spencer Schack <scs1818@gmail.com>
2013-01-09 12:07:52 -0800

    new test scores layout

--- app/helpers/test_scores_helper.rb
+++ app/helpers/test_scores_helper.rb
@@ -26,8 +26,8 @@ module TestScoresHelper
       hash[test_name] = {}
       test_scores.sort_by(&:term).each_with_index do |score, index|
         score.data.each do |key, value|
-          if index.zero? || (!level_column?(key) && key !~ /_rc/)
-            hash[test_name]["#{key} #{Term.shorten(score.term)}"] = {
+          if !level_column?(key) && (index.zero? || key !~ /_rc/)
+            hash[test_name]["#{key.titleize} #{Term.shorten(score.term)}"] = {
               level: score.data[level_column_for(key)],
               score: value
             }
