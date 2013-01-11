f7e0cf36fe859af6415c50e7fd43920cb5d5e820
Spencer Schack <scs1818@gmail.com>
2013-01-10 16:41:02 -0800

    removed student count from periods index

--- app/helpers/periods_helper.rb
+++ app/helpers/periods_helper.rb
@@ -7,7 +7,7 @@ module PeriodsHelper
   }
   
   FIELDS[:periods] = {
-    index: [:name, :student_count, :school],
+    index: [:name, :school],
     show: { fields: [:name, :term], relations: [:school, :students, :users, :test_scores]},
     form: { fields: [:name, [:term, collection: Term.options_for_select]],
       relations: [[:school, as: :search_select], [:students, as: :token, depends_on: :school],
