1899bcebb4e59b97791d127f25ad1884e9b6f716
Spencer Schack <scs1818@gmail.com>
2013-01-24 20:57:52 -0800

    added caching to Setting

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -6,13 +6,32 @@ class Setting < ActiveRecord::Base
   
   validates_presence_of :key, :value
   validates_uniqueness_of :key
+  before_save :delete_from_cache
+  after_save :write_to_cache
+  before_destroy :delete_from_cache
   
   def self.value_of key
-    where(key: key).first.try(:value)
+    Rails.cache.fetch(cache_key_for(key)) do
+      where(key: key).first.try(:value)
+    end
   end
   
   def name
     key
+  end
+  
+  private
+  
+  def self.cache_key_for key
+    "settings/#{key}"
+  end
+  
+  def delete_from_cache
+    Rails.cache.delete(self.cache_key_for(key_was))
+  end
+  
+  def write_to_cache
+    Rails.cache.write(self.cache_key_for(key), value)
   end
   
 end
